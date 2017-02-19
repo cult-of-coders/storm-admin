f3800c025bf75fc19f18935e598bdc36d8066ea2
prodevelsol <contacts@prodevel.solutions>
2017-02-19 23:06:30 +0100

    fixed issue with space and ',' in array view

--- lib/ui/lib/FieldDisplays/ArrayField.jsx
+++ lib/ui/lib/FieldDisplays/ArrayField.jsx
@@ -6,9 +6,9 @@ export default function ArrayField({ value }) {
             {
                 _.map(value, (element) => {
                     const id = _.uniqueId();
-                    if (_.last(value, element)) {
+                    if (_.last(value) === element) {
                         return (
-                            <span key={id}>{element}</span>
+                            <span key={id}>{element} </span>
                         );
                     }
                     return (
